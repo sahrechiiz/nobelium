d367ea2881f53d2377a17dd83b70df2242bce822
多多河 <sahrechiiz@gmail.com>
2022-09-17 01:33:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: '多多河流域',
+  author: 'dodoho',
+  email: 'i@dodoho.io',
+  link: 'https://blog.dodoho.io',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
